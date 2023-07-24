528d7284da02808c6cf67a698a0b52f8873b7306
Raymond <raymondurrutia2@yahoo.com>
2023-07-24 11:06:14 -0400

    Updated type effect to TypewriterJS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import Typed from "react-typed";
 import { useInView } from 'react-intersection-observer';
+import Typewriter from "typewriter-effect";
 import { FaLaptopCode } from "react-icons/fa";
 
 export default function Header() {
@@ -16,17 +16,14 @@ export default function Header() {
                 <FaLaptopCode className="header-icon" />
                 <h1>Raymond Urrutia</h1>
 
-                {/* - "strings" is the array with the text you want to see typed
-                - typeSpeed is the speed you want the text typed out
-                - backSpeed is the speed you want the text typed back
-                */}
-                <Typed
-                    className="typed-text"
-                    strings={["Web Development", "Web Design", "UX Design", "Graphic Design"]}
-                    typeSpeed={50}
-                    backSpeed={60}
-                    loop
+                <Typewriter
+                    options={{
+                        strings: ["Web Development", "Web Design", "UX Design", "Graphic Design"],
+                        autoStart: true,
+                        loop: true,
+                    }}
                 />
+
             </div>
         </div>
     )
